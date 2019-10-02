8777d9486aa0c487a4256dd2f4d581b917c54647
Carlos Veloz <carlos_veloz@MacBook-Pro-76.local>
2019-10-02 10:39:26 -0500

    changing script names and adding eslint

--- pages/todoPage.js
+++ pages/todoPage.js
@@ -1,46 +1,45 @@
-module.exports =  {
-    commands: [{
-        addTask(taskString){
-            return this.waitForElementVisible('@toDoTextField')
-                .setValue('@toDoTextField', taskString)
-                .sendKeys('@toDoTextField',this.api.Keys.ENTER)
-                .waitForElementVisible('@counter');
-        },
-        toggleFirstElement(){
-            return this.waitForElementVisible('@list')
-                .click('@firstElementToggle');
-        },
-        validates(element, items){
-            //return this.assert.containsText(element, items)
-            return this.getText(element, function(result){
-                this.assert.equal(result.value, items);
-            });
-        }
-    }],
-    url(){
-        return this.api.launchUrl;
+module.exports = {
+  commands: [{
+    addTask(taskString) {
+      return this.waitForElementVisible('@toDoTextField')
+        .setValue('@toDoTextField', taskString)
+        .sendKeys('@toDoTextField', this.api.Keys.ENTER)
+        .waitForElementVisible('@counter');
     },
-    elements: {
-        toDoTextField: {
-            selector: '.new-todo'
-        },
-        toDoForm: {
-            selector: '.todo-form'
-        },
-        header: {
-            selector: '.header h1'
-        },
-        list: {
-            selector: '.todo-list li'
-        },
-        firstElementToggle: {
-            selector: '.todo-list li:first-child .toggle'
-        },
-        completed: {
-            selector: '.clear-completed'
-        },
-        counter: {
-            selector: '.todo-count .ng-binding'
-        }
+    toggleFirstElement() {
+      return this.waitForElementVisible('@list')
+        .click('@firstElementToggle');
+    },
+    validates(element, items) {
+      return this.getText(element, function(result) {
+        this.assert.equal(result.value, items);
+      });
     }
+  }],
+  url() {
+    return this.api.launchUrl;
+  },
+  elements: {
+    toDoTextField: {
+      selector: '.new-todo'
+    },
+    toDoForm: {
+      selector: '.todo-form'
+    },
+    header: {
+      selector: '.header h1'
+    },
+    list: {
+      selector: '.todo-list li'
+    },
+    firstElementToggle: {
+      selector: '.todo-list li:first-child .toggle'
+    },
+    completed: {
+      selector: '.clear-completed'
+    },
+    counter: {
+      selector: '.todo-count .ng-binding'
+    }
+  }
 };
--- tests/overview.spec.js
+++ tests/overview.spec.js
@@ -3,35 +3,35 @@ var todo;
 
 module.exports = {
 
-    beforeEach : function(browser) {
-      todo = browser.page.todoPage();
-      todo.navigate();
-    },
+  beforeEach: function(browser) {
+    todo = browser.page.todoPage();
+    todo.navigate();
+  },
 
-    afterEach : function() {
-      todo.end();
-    },
+  afterEach: function() {
+    todo.end();
+  },
 
-    'There are not task in the list': function () {
-      todo.waitForElementVisible('@header')
-        .assert.elementNotPresent('@list');
-    },
+  'There are not task in the list': function() {
+    todo.waitForElementVisible('@header')
+      .assert.elementNotPresent('@list');
+  },
 
-    'Add a new task to the list': function (){
-      todo.addTask(inputData.task)
+  'Add a new task to the list': function() {
+    todo.addTask(inputData.task)
       .validates('@counter', inputData.oneItem);
-    },
+  },
 
-    'Remove completed tasks': function () {
-      todo.addTask(inputData.task).addTask(inputData.task).toggleFirstElement()
+  'Remove completed tasks': function() {
+    todo.addTask(inputData.task).addTask(inputData.task).toggleFirstElement()
       .validates('@counter', inputData.oneItem);
-      todo.click('@completed').validates('@counter', inputData.oneItem);
-    },
+    todo.click('@completed').validates('@counter', inputData.oneItem);
+  },
 
-    'After I add a task and toggle it there should not be items left': function () {
-      todo.addTask(inputData.task).toggleFirstElement()
+  'After I add a task and toggle it there should not be items left': function() {
+    todo.addTask(inputData.task).toggleFirstElement()
       .validates('@counter', inputData.zeroItems);
-      todo.validates('@completed', inputData.clearCompleted);
-    }
-    
+    todo.validates('@completed', inputData.clearCompleted);
+  }
+
 };
